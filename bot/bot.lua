32c54fa9133d13c107afaac9438032f13c69c0e4
mirbot1122 <mirbot1122@gmail.com>
2015-11-26 11:16:19 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {129070606,129820298},
+    sudo_users = {129070606,178263682},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
